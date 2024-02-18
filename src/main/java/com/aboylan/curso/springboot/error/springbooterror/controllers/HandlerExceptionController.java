ddbb52edb32bf9353c5929e724cbc436d6fe13e2
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-17 19:52:58 -0600

    Anadiendo metodo handler para el manejo de excepcion

--- src/main/java/com/aboylan/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/aboylan/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -1,16 +1,27 @@
 package com.aboylan.curso.springboot.error.springbooterror.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.aboylan.curso.springboot.error.springbooterror.models.Error;
+
 @RestControllerAdvice
 public class HandlerExceptionController {
 
-    @ExceptionHandler({ArithmeticException.class})
-    public ResponseEntity<?> divisionByZero(Exception ex) {
+    @ExceptionHandler({ ArithmeticException.class })
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error division por cero!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
-        return ResponseEntity.internalServerError().body("Error 500");
+        // return ResponseEntity.internalServerError().body(error);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
 }
