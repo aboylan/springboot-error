e0e2d9c64644668451e626d7f24d1c7e29d88ad0
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-19 18:58:13 -0600

    Personalizando error 404

--- src/main/java/com/aboylan/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ src/main/java/com/aboylan/curso/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.aboylan.curso.springboot.error.springbooterror.models.Error;
 
@@ -24,4 +25,14 @@ public class HandlerExceptionController {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error division por cero!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
 }
